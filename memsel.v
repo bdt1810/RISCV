b63c8e5ad840c82868c11fc0933ccc175f92a846
bdt1810 <66118326+bdt1810@users.noreply.github.com>
2020-09-21 11:33:32 +0700

    Update memsel.v

--- memsel.v
+++ memsel.v
@@ -9,7 +9,7 @@ module memsel(memsel_out, Memsel, DataR);
 	input [n-1:0] DataR;
 	output [n-1:0] memsel_out;
 
-	assign memsel_out = (Memsel === `LW)  ? in :
+	assign memsel_out = (Memsel === `LW)  ? DataR :
 		     (Memsel === `LH)  ? {{16{DataR[15]}},DataR[15:0]} :
 		     (Memsel === `LB)  ? {{24{DataR[7]}},DataR[7:0]}   :
 		     (Memsel === `LHU) ? {{16{1'b0}},DataR[15:0]}   :
