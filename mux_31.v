6fe99b27133e2bdf03c416b850d495b2aa57a4f3
nptAnnn <71619631+nptAnnn@users.noreply.github.com>
2020-09-21 11:48:42 +0700

    Update mux_31.v

--- mux_31.v
+++ mux_31.v
@@ -1,14 +1,14 @@
 `define ALU  2'b01
 `define PC4   2'b10
 `define DMEM 2'b00
-module mux31 (DataD, WBSel, pc4, ALU_Out, memsel_out);
+module mux31(DataD, WBSel, pc4, ALU_Out, memsel_out);
 	parameter n = 32;
 	input [n-1:0] pc4, ALU_Out, memsel_out;
 	input [1:0] WBSel;
-	output [n-1:0] WB;
+	output [n-1:0] DataD;
 	
 
-	assign WB = (WBSel == `DMEM) ? memsel_out:
+	assign DataD = (WBSel == `DMEM) ? memsel_out:
 		(WBSel == `ALU) ? ALU_Out:
 		(WBSel == `PC4) ? pc4:
 		32'b0;
