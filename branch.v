bb09bcac2f6ae1b934950b8790d8c9133b2c7395
nptAnnn <71619631+nptAnnn@users.noreply.github.com>
2020-09-21 11:12:51 +0700

    Update branch.v

--- branch.v
+++ branch.v
@@ -1,19 +1,17 @@
-module branch(in0, in1, BrUn, BrEq, BrLT);
+module branch(BrEq, BrLT, DataA, DataB, BrUn);
 	parameter n = 32;
 	input BrUn;
-	input [n-1:0] in0, in1;
+	input [n-1:0] DataA, DataB;
 	output reg BrEq, BrLT; 
 	assign BrUn = 1'b0;
-	assign in0 = 32'h00000001;
-	assign in1 = 32'h80000001;
 	always @(*) begin
 		if (BrUn === 1'b1) begin
-			BrEq = (in0 === in1) ? 1 : 0;
-			BrLT = (in0 < in1) ? 1: 0; 
+			BrEq = (DataA === DataB) ? 1 : 0;
+			BrLT = (DataA < DataB) ? 1: 0; 
 		end
 		else begin 
-			BrEq = (in0 === in1) ? 1 : 0;
-			BrLT = ($signed(in0) < $signed(in1)) ? 1: 0; 
+			BrEq = (DataA === DataB) ? 1 : 0;
+			BrLT = ($signed(DataA) < $signed(DataB)) ? 1: 0; 
 		end
 	end
 endmodule
