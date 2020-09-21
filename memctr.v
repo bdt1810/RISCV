389992dba136ddbe6414a4a9e598a583e225b344
nptAnnn <71619631+nptAnnn@users.noreply.github.com>
2020-09-21 11:18:45 +0700

    Update memctr.v

--- memctr.v
+++ memctr.v
@@ -1,13 +1,13 @@
 `define SW	2'b00
 `define SH	2'b01
 `define SB	2'b00
-module memctr(in, out, MemCtr);
+module memctr(DataW, MemCtrl, DataB);
 	parameter n = 32; 
-	input [n-1:0] in;
-	output [n-1:0] out;  
+	input [n-1:0] DataB;
+	output [n-1:0] DataW;  
 	output [1:0] MemCtr;
 
-	assign out = (MemCtr === `SW)  ? in :
-		     (MemCtr === `SH)  ? {{16{in[15]}},in[15:0]} :
-		     (MemCtr === `SB)  ? {{24{in[7]}},in[7:0]}   : 32'h0000_0000;	
+	assign DataW = (MemCtr === `SW)  ? DataB :
+		     (MemCtr === `SH)  ? {{16{DataB[15]}},DataB[15:0]} :
+		     (MemCtr === `SB)  ? {{24{DataB[7]}},DataB[7:0]}   : 32'h0000_0000;	
 endmodule 
