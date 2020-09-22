87d962d16f024e53f2e5f1dbc650e3c5878c2990
nptAnnn <71619631+nptAnnn@users.noreply.github.com>
2020-09-22 21:37:45 +0700

    Update mapping.v

--- mapping.v
+++ mapping.v
@@ -13,12 +13,12 @@
 
 module RISCV(clk);
 input clk;
-wire[31:0] pc_out, inst, ALU_Result, ALU_Out, DataR, DataA, DataB, DataD, tmp2, tmp3, DataW, pc_in, pc_4, imm_out, memsel_out;
+wire[31:0] pc_out, inst, ALU_Result, ALU_Out, DataR, DataA, DataB, DataD, tmpA, tmpB, DataW, pc_in, pc_4, imm_out, memsel_out;
 wire[4:0] AddrD, AddrA, AddrB;
 wire[3:0] ALU_Sel;
 wire[2:0] ImmSel, MemSel;
 wire[1:0] WBSel, MemCtr;
-wire RegWEn, BrEq, BrLT, PCsel, BrUN, Asel, Bsel, MemRW;
+wire RegWEn, BrEq, BrLT, PCsel, BrUN, ASel, BSel, MemRW;
 wire [8:0] instrom;
 
 wire [24:0] imm_in;
@@ -28,21 +28,20 @@ assign AddrA = {inst[19:15]};
 assign AddrB = {inst[24:20]};
 assign AddrD = {inst[11:7]};
 assign instrom = {inst[30],inst[14:12],inst[6:2]};
+assign imm_in = {inst[25:0]};
 
 mux21 PCmux(pc_in, PCsel, ALU_Out, pc_4);
 pc pc(pc_out, pc_in, clk);
 pc4 pc4(pc_4, pc_out);
 imem imem(inst, pc_out);
-
 register register(AddrD,AddrA,AddrB,DataD,RegWEn,DataA,DataB,clk);
-
 branch branch(BrEq, BrLT, DataA, DataB, BrUn);
 mux21 muxA(tmpA, ASel, DataA, pc_out);
 mux21 muxB(tmpB, BSel, DataB, imm_out);
 se se(imm_out,ImmSel, imm_in);
 alu alu(ALU_Out, ALU_Sel, tmpA, tmpB);
 mux31 mux31(DataD,WBSel, pc_4, ALU_Out, memsel_out);
-rom rom(instrom, BrEq, BrLT, PCsel, ImmSel, BrUN, Asel, Bsel, ALU_Sel, MemRW, RegWEn, MemSel, MemCtr, WBSel);
+rom rom(instrom, BrEq, BrLT, PCsel, ImmSel, BrUN, ASel, BSel, ALU_Sel, MemRW, RegWEn, MemSel, MemCtr, WBSel);
 dmem dmem(DataR, ALU_Out, MemRW, DataW, clk );
 memctr memctr(DataW, MemCtr, DataB);
 memsel memsel(memsel_out, MemSel, DataR);	
