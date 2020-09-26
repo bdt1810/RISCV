3643a2670b56d68225fe7604d4d1e9bbae878a50
nptAnnn <71619631+nptAnnn@users.noreply.github.com>
2020-09-26 13:37:49 +0700

    Update alu.v

--- alu.v
+++ alu.v
@@ -1,33 +1,31 @@
 module alu(ALU_Out, ALU_Sel, tmpA, tmpB);
     input [31:0] tmpA, tmpB;  // ALU 32-bit Inputs                 
     input [3:0] ALU_Sel;// ALU Selection
-    output [31:0] ALU_Out;	
-    reg [31:0] ALU_Result;
-    assign ALU_Out = ALU_Result; // ALU out
+    output reg[31:0] ALU_Out;	
     always @(*)
     begin
         case(ALU_Sel)
         4'b0000: // Addition
-           	ALU_Result = tmpA + tmpB ; 
+           	ALU_Out = tmpA + tmpB ; 
         4'b0001: // Subtraction
-           	ALU_Result = tmpA - tmpB ;
+           	ALU_Out = tmpA - tmpB ;
         4'b0010: //  Logical and 
-           	ALU_Result = tmpA & tmpB;
+           	ALU_Out = tmpA & tmpB;
         4'b0011: //  Logical or
-           	ALU_Result = tmpA | tmpB;
+           	ALU_Out = tmpA | tmpB;
         4'b0100: //  Logical xor 
-           	ALU_Result = tmpA ^ tmpB;
+           	ALU_Out = tmpA ^ tmpB;
 	4'b0101: //  Shift left logical
-	   	ALU_Result = tmpA << tmpB;
+	   	ALU_Out = tmpA << tmpB;
 	4'b0110: // Shift right logical
-	   	ALU_Result = tmpA >> tmpB;
+	   	ALU_Out = tmpA >> tmpB;
 	4'b0111: // Shitf right arithmetic with A signed;
-		ALU_Result = $signed(tmpA) >>> $unsigned(tmpB);
+		ALU_Out = $signed(tmpA) >>> $unsigned(tmpB);
 	4'b1001: // Signed compare 
-		ALU_Result = ($signed(tmpA)<$signed(tmpB))?32'b1:32'b0;
+		ALU_Out = ($signed(tmpA) < $signed(tmpB)) ? 32'd1:32'b0;
 	4'b1010: // Unsigned compare
-		ALU_Result = ($unsigned(tmpA) < $unsigned (tmpB)) ? 32'b1:32'b0;
-        default: ALU_Result = 32'h0; 
+		ALU_Out = ($unsigned(tmpA) < $unsigned (tmpB)) ? 32'd1:32'b0;
+        default: ALU_Out = 32'd0; 
         endcase
     end
 
