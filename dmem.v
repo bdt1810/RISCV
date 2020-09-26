49d01761bc5086cdcde762b87a7e86e3849d4ac2
nptAnnn <71619631+nptAnnn@users.noreply.github.com>
2020-09-26 13:38:49 +0700

    Update dmem.v

--- dmem.v
+++ dmem.v
@@ -1,20 +1,26 @@
 module dmem(DataR, ALU_Out, MemRW, DataW, clk );
 	parameter n = 32;
-	parameter a = 1<<27;
+	parameter a = 1<<20;
 	input MemRW, clk;
 	input [n-1:0] ALU_Out;
 	input [n-1:0] DataW;
 	output reg[n-1:0] DataR;
 	reg [n-1:0]DMEM[0:a-1];
-
-
+	integer i;
+initial 
+begin
+	for (i=0; i<a; i=i+1)
+		DMEM[i] <= 32'h0;	
+end
 	always @(posedge clk)
 	begin
-	case(MemRW)
-	1'b1: 
+	if (MemRW === 1'b1)
+		begin
 		DMEM[ALU_Out] <= DataW;
-	1'b0: 
-		DataR <= DMEM[ALU_Out]; 
-	endcase
+		end
 	end
+always @(ALU_Out)
+begin
+	DataR <= DMEM[ALU_Out];
+end
 endmodule
