b1da5d39637effb8853a4e3927a4fec96315dd1d
nptAnnn <71619631+nptAnnn@users.noreply.github.com>
2020-09-28 10:25:42 +0700

    Update branch.v

--- branch.v
+++ branch.v
@@ -3,7 +3,6 @@ module branch(BrEq, BrLT, DataA, DataB, BrUn);
 	input BrUn;
 	input [n-1:0] DataA, DataB;
 	output reg BrEq, BrLT; 
-	assign BrUn = 1'b0;
 	always @(*) begin
 		if (BrUn === 1'b1) begin
 			BrEq = (DataA === DataB) ? 1 : 0;
