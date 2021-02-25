e8fe89118c75e3c9e45d7c6c1dbaeb42348e0382
nptAnnn <71619631+nptAnnn@users.noreply.github.com>
2021-02-25 10:35:20 +0700

    Update taken.v

--- taken.v
+++ taken.v
@@ -1,13 +1,18 @@
-module taken(taken1, taken2, inst, predict1, predict2, PCsel, clk);
+module taken(true, taken1, taken2, inst, prediction, predict1, predict2, PCsel, clk);
 input [31:0] inst;
-input predict1, predict2, PCsel, clk;
-output reg taken1, taken2;
-always @(negedge clk)
+input prediction, predict1, predict2, PCsel, clk;
+output reg taken1, taken2, true;
+always @(posedge clk)
 begin
 	if (inst[6:0] === 7'b1100011)
 	begin
 		taken1 <= (PCsel === predict1) ? 1'b0 : 1'b1;
 		taken2 <= (PCsel === predict2) ? 1'b0 : 1'b1;
+		true <= (PCsel === prediction) ? 1'b0 : 1'b1;
 	end
+	else 
+	begin
+		true <= 1'b0;
+	end 
 end
 endmodule
