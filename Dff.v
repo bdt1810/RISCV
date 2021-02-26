2f2db61efaefcb48026c91ba1c2403273ef0b764
nptAnnn <71619631+nptAnnn@users.noreply.github.com>
2021-02-26 16:28:01 +0700

    Update Dff.v

--- Dff.v
+++ Dff.v
@@ -1,15 +1,22 @@
-module Dff(out, clear, in, clk);
-	parameter n = 32;
-	input [n-1:0] in;
-	input clear; // 1 is clear, 0 is not clear
+module Dff(out, clear, in1, in2, clk);
+	input [31:0] in1;
+	input [31:0] in2;
+	input [1:0] clear; 
 	input clk;
-	output reg [n-1:0] out;
-initial	
+	output reg[31:0] out;
+
+	initial	
 	begin
-		out <= 32'h0;
+		 out <= 32'h0;
 	end
-always @(posedge clk)
+
+	always @(posedge clk)
 	begin
-		out <= clear? 32'd0 : in;
+		case(clear)
+		2'b00: out <= in1;	// 00 is normal
+		2'b01: out <= in2;	// 01 is stall
+		2'b10: out <= in2;	// 10 is stall
+		2'b11: out <= 32'd0; 	// 11 is flush
+		endcase
 	end
 endmodule
